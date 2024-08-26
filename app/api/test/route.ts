fdcc44a47a768440129438280ea8fa67910e7696
Martin <moragamartin2@gmail.com>
2024-08-26 10:02:22 -0300

    test update

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,12 +1,12 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '../../lib/prisma'; // Adjust the path if needed
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/app/lib/prisma'; // Adjust the path if needed
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest) {
   try {
     // Attempt to connect to the database
     await prisma.$queryRaw`SELECT 1`;
-    res.status(200).json({ success: true, message: 'Connected to the database successfully!' });
-  } catch (error : any) {
-    res.status(500).json({ success: false, message: `Failed to connect to the database: ${error.message}` });
+    return NextResponse.json({ success: true, message: 'Connected to the database successfully!' });
+  } catch (error: any) {
+    return NextResponse.json({ success: false, message: `Failed to connect to the database: ${error.message}` }, { status: 500 });
   }
 }

2efc695b243ccecb30a0df79f974d96831fd647e
martinmorag <113954312+martinmorag@users.noreply.github.com>
2024-11-25 00:51:07 -0300

    Add files via upload

---
+++ app/panel/anuncios/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AnunciosPage from "@/app/ui/anuncios/AnunciosPage";
+
+const Page : React.FC = () => {
+    return <AnunciosPage />
+}
+
+export default Page;
---
+++ app/panel/perfil/seguridad/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EditSecurity from "@/app/ui/perfil/EditSecurity";
+
+const Page : React.FC = () => {
+    return (
+        <div className="h-[100%] min-w-[50%] w-[100%]">
+            <EditSecurity />
+        </div>
+    )
+}
+
+export default Page;

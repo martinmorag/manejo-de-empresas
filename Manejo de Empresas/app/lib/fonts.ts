b32ee55f0612c19d1e2036900a350db2b5050d0c
martinmorag <113954312+martinmorag@users.noreply.github.com>
2024-11-25 00:40:22 -0300

    Delete Manejo de Empresas directory

--- Manejo de Empresas/app/lib/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Inter } from "next/font/google";
-import { Nunito } from "next/font/google";
-
-export const nunito = Nunito({ subsets: ["latin"], weight: ["400", "700"] });
-export const inter = Inter({ subsets: ["latin"] });
--- Manejo de Empresas/app/panel/anuncios/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AnunciosPage from "@/app/ui/anuncios/AnunciosPage";
-
-const Page : React.FC = () => {
-    return <AnunciosPage />
-}
-
-export default Page;
